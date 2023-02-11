b36097e7c667aa2b1e728beb469240d19ed5898e
prolate-meteors <prolate.meteors@gmail.com>
2023-02-12 01:03:05 +0200

    UPdates

--- src/mobile.js
+++ src/mobile.js
@@ -10,18 +10,3 @@ menuOpen.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
   overlay.classList.remove("overlay--active");
 });
-
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  grabCursor: true,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
